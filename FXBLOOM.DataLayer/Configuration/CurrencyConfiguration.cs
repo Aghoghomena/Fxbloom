6b6877b50bc7772cd316f6399ef0c73323bf659d
dhamie <dhamie.soyemi@gmail.com>
2021-05-28 12:46:41 +0100

    Add project files.

---
+++ FXBLOOM.DataLayer/Configuration/CurrencyConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using FXBLOOM.DomainLayer.CustomerAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FXBLOOM.DataLayer.Configuration
+{
+    class CurrencyConfiguration : IEntityTypeConfiguration<Currency>
+    {
+        public void Configure(EntityTypeBuilder<Currency> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.CurrencyType).HasMaxLength(500).IsRequired();
+            builder.Property(e => e.Amount).IsRequired();
+
+
+        }
+    
+    }
+}
---
+++ FXBLOOM.DomainLayer/CustomerAggregate/Currency.cs
@@ -0,0 +1,32 @@
+﻿using FXBLOOM.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using static FXBLOOM.SharedKernel.Enumerations;
+
+namespace FXBLOOM.DomainLayer.CustomerAggregate
+{
+    public class Currency:ValueObject<Currency>
+    {
+        public CurrencyType CurrencyType { get; private set; }
+
+        public decimal Amount { get; private set; }
+
+        public int Id { get; set; }
+
+        internal static Currency CreateCurrency(CurrencyType type, decimal amount)
+        {
+            Currency currency = new Currency();
+            currency.CurrencyType = type;
+            currency.Amount = amount;
+
+            return currency;
+        }
+
+        internal void UpdateAmount(decimal amount)
+        {
+            Amount = amount;
+        }
+    }
+}
